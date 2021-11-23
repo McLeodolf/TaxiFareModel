397d1d96621aefaf8dd590da115814885492f4bc
Leo Allgaier <leo.allgaier@gmx.net>
2021-11-23 16:36:20 +0000

    added tests

--- tests/test_trainer.py
+++ tests/test_trainer.py
@@ -1,5 +1,11 @@
 from TaxiFareModel.trainer import Trainer
+from TaxiFareModel.data import get_data, clean_data
+import pandas as pd
 
 def test_trainer():
-    trainer = Trainer()
-    assert isinstance(trainer, Trainer())
+    df = clean_data(get_data())
+    y = df.pop("fare_amount")
+    X = df
+    assert isinstance(df, pd.DataFrame)
+    trainer = Trainer(X,y)
+    assert isinstance(trainer, Trainer)
